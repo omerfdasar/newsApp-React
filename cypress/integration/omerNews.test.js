73d15b2860b57b295ccfea566d59fdc13f18cf75
Omer <omerfarukdasar@gmail.com>
2022-04-22 14:06:24 +0200

    url test

--- cypress/integration/omerNews.test.js
+++ cypress/integration/omerNews.test.js
@@ -1,5 +1,36 @@
 describe("Header test", () => {
+  beforeEach(() => {
+    const url =
+      "https://newsapi.org/v2/everything?" +
+      "q=Apple&" +
+      "from=2022-04-18&" +
+      "sortBy=popularity&" +
+      "apiKey=1a1a999e0d7240a6bd2dead87bcca78e";
+
+    cy.request(url).then((res) => {
+      cy.log("response: ", res);
+    });
+  });
+
   it("Omer events", () => {
-    console.log("first");
+    cy.log("start");
+    cy.visit("http://localhost:3000/");
+    cy.wait(2000);
+    // Query for an element
+    cy.contains("Omer");
+  });
+
+  it("Register control", () => {
+    cy.contains("Register").click().wait(2000);
+
+    cy.url().should("include", "http://localhost:3000/register");
+  });
+  it("typing", () => {
+    cy.visit("http://localhost:3000/login");
+    cy.get("#email")
+      .type("test@gmail.com")
+      .should("have.value", "test@gmail.com");
+    cy.get("input[name='password']").type("verysecret");
+    cy.get("button").contains("Login");
   });
 });
