86311a27c5d5c6508a10c7de143b644a7ed04835
Omer <omerfarukdasar@gmail.com>
2022-04-22 15:29:12 +0200

    register control

--- cypress/integration/omerNews.test.js
+++ cypress/integration/omerNews.test.js
@@ -14,23 +14,46 @@ describe("Header test", () => {
 
   it("Omer events", () => {
     cy.log("start");
-    cy.visit("http://localhost:3000/");
+    cy.visit("http://localhost:3000");
     cy.wait(2000);
     // Query for an element
     cy.contains("Omer");
   });
 
   it("Register control", () => {
-    cy.contains("Register").click().wait(2000);
-
+    cy.visit("http://localhost:3000/register");
+    cy.contains("Register").click();
     cy.url().should("include", "http://localhost:3000/register");
   });
-  it("typing", () => {
-    cy.visit("http://localhost:3000/login");
-    cy.get("#email")
-      .type("test@gmail.com")
-      .should("have.value", "test@gmail.com");
-    cy.get("input[name='password']").type("verysecret");
-    cy.get("button").contains("Login");
+
+  it("registering email", () => {
+    let chars = "abcdefghijklmnopqrstuvwxyz1234567890";
+    let string = "";
+    for (var ii = 0; ii < 15; ii++) {
+      string += chars[Math.floor(Math.random() * chars.length)];
+    }
+    cy.log(string + "@domain.com");
+    var email = string + "@domain.com";
+    cy.contains("Register").click();
+
+    cy.get("#email").type(email).should("have.value", email);
+    cy.get("input[name='password']").type(email);
+    cy.get("body")
+      .children("div")
+      .children("div")
+      .children("div")
+      .eq(1)
+      .children("div div")
+      .children("form")
+      .contains("Register")
+      .click();
   });
+
+  //   it("type email", () => {
+  //     cy.visit("http://localhost:3000/login");
+  //     cy.get("#email").type("m123@gmail.com");
+  //     cy.get('input[type="password"]').type("m123456");
+  //     // click button
+  //     cy.get("button").contains("Login").click();
+  //   });
 });
